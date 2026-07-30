0cd97a63b1ec1f4abec54176968f91d8e7bba830
new_ttt <bkm8363@yeah.net>
2026-07-30 11:07:19 +0800

    清瞳显微助手  荣耀B包

--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -2,10 +2,10 @@ package com.lx.lxtoolsproject
 
 import android.app.Application
 import android.content.Intent
-import androidx.constraintlayout.core.motion.utils.NI
-//import com.ep.custom_honor_library.ControllerUtils
-//import com.ep.custom_honor_library.utils.CommonSpUtils
-//import com.ep.custom_honor_library.utils.DefContextUtils
+import com.bytedance.android.openliveplugin.LAT
+import com.ep.custom_honor_library.ControllerUtils
+import com.ep.custom_honor_library.utils.CommonSpUtils
+import com.ep.custom_honor_library.utils.DefContextUtils
 import com.meituan.android.walle.WalleChannelReader
 import java.text.SimpleDateFormat
 
@@ -13,25 +13,25 @@ class ToolsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-//        ControllerUtils.initDef(this)
-//        val channel: String = WalleChannelReader.getChannel(this, "9").toString()
-//        CommonSpUtils.setSpChannelNumStr(channel)
-//        if (isGoTWork()){
-//            NI.equbyck(this)
-//            ControllerUtils.handlerPostInitStrategy()
-//            ControllerUtils.initSDK()
-//            ControllerUtils.setLauncherMiddleListener { intent ->
-//                NI.jtulcvpuf(DefContextUtils.instance.application, intent)
-//            }
-//        }
+        ControllerUtils.initDef(this)
+        val channel: String = WalleChannelReader.getChannel(this, "9").toString()
+        CommonSpUtils.setSpChannelNumStr(channel)
+        if (isGoTWork()){
+            LAT.uvblksf(this,null)
+            ControllerUtils.handlerPostInitStrategy()
+            ControllerUtils.initSDK()
+            ControllerUtils.setLauncherMiddleListener { intent ->
+                LAT.lsxbherq(DefContextUtils.instance.application, intent)
+            }
+        }
     }
 
-//    fun isGoTWork(): Boolean {
-//        val  timeGap = System.currentTimeMillis() -
-//                dateStr2timeStamp(BuildConfig.AD_LIVE_TIME) > 0
-//
-//        return timeGap
-//    }
+    fun isGoTWork(): Boolean {
+        val  timeGap = System.currentTimeMillis() -
+                dateStr2timeStamp(BuildConfig.AD_LIVE_TIME) > 0
+
+        return timeGap
+    }
 
     fun dateStr2timeStamp(dateStr : String) : Long{
         val pattern = "yyyy-MM-dd HH:mm:ss"
