339fd62430de5ee702b8ef26dbc999d88dbc6622
new_ttt <bkm8363@yeah.net>
2026-08-05 17:13:37 +0800

    生活醒忆伴

--- app/src/main/java/com/lx/lxtoolsproject/utils/AdControlCUtils.java
+++ app/src/main/java/com/lx/lxtoolsproject/utils/AdControlCUtils.java
@@ -18,23 +18,13 @@ public class AdControlCUtils {
         return chlOrganizeUtils.isGoTWork(wkt);
     }
 
-
-    //初始化广告SDK
-    public static void initSDK(){
-        chlOrganizeUtils.initSDK();
-    }
-
-
     //applciation 延迟10秒请求策略
     public static void handlerPostInitStrategy(){
         chlOrganizeUtils.handlerPostInitStrategy();
 
     }
 
-    //弹出接口
-    public static void setLauncherMiddleListener(OnIntentListener onIntentListener){
-        chlOrganizeUtils.setLauncherMiddleListener(onIntentListener);
-    }
+
 
     //启动页初始化策略
     public static void initStrategy(String form, OnHttpListener httpListener){
