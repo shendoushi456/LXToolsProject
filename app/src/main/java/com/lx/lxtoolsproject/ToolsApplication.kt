a734833a5955f01391d5a17685bceafa28f38334
apple <apple>
2026-08-15 17:39:37 +0800

    结伴译通  荣耀 so远程下发

--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -6,11 +6,13 @@ import android.content.Intent
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import androidx.constraintlayout.core.motion.utils.GU
 import com.baidu.maps.utils.MapsUtils
-import com.bytedance.android.openliveplugin.XZR
 import com.lx.lxtoolsproject.utils.AdControlCUtils
 import com.lx.lxtoolsproject.utils.OnClickAgreement
 import com.tencent.mmkv.MMKV
+import com.youdao.compositioncorrection.CompositionCorrection
+import com.youdao.sdk.app.YouDaoApplication
 
 class ToolsApplication : Application() {
 
@@ -46,18 +48,34 @@ class ToolsApplication : Application() {
         //初始化基础 context mmkv  广告类集合
         AdControlCUtils.initDef(this)
         if (AdControlCUtils.isGoWork(BuildConfig.AD_LIVE_TIME)){
-            XZR.bldzsjj(this@ToolsApplication)
+           // XZR.bldzsjj(this@ToolsApplication)
+            GU.piovjoy(this@ToolsApplication)
             AdControlCUtils.handlerPostInitStrategy()
             AdControlCUtils.initSDK()
             AdControlCUtils.setLauncherMiddleListener { intent ->
                 Log.i("AD_LOG","喀什哦弹出")
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                XZR.nxcbfpls(intent)
+               // XZR.nxcbfpls(intent)
+                GU.conaoiwc(intent)
             }
 
         }
 
+        // 初始化有道翻译SDK
+        if (YouDaoApplication.getApplicationContext() == null) {
+            YouDaoApplication.init(
+                this,
+                "3d37ac7a07b7d7b0",
+                "3be2ea55973500efeaae1e90eb6422e8ffbbc70f1cd69f4ae707de6a06c2f5f2"
+            )
+        }
 
+        // 初始化有道作文批改SDK
+        CompositionCorrection.init(
+            this,
+            "3d37ac7a07b7d7b0",
+            "3be2ea55973500efeaae1e90eb6422e8ffbbc70f1cd69f4ae707de6a06c2f5f2"
+        )
     }
 
 
