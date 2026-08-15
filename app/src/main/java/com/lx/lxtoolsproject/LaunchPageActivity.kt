878a3695762c83faf0c706e592372ae0c2f1dd0b
new_ttt <bkm8363@yeah.net>
2026-08-15 16:28:50 +0800

    译典万言通 l武汉版本SDK

--- app/src/main/java/com/lx/lxtoolsproject/LaunchPageActivity.kt
+++ app/src/main/java/com/lx/lxtoolsproject/LaunchPageActivity.kt
@@ -7,7 +7,7 @@ import android.os.Handler
 import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import com.lx.lxtoolsproject.databinding.LaunchPageActivityBinding
-import com.p.a_b.ui.main.MainMobileDataActivity
+import com.xian.bc.accounts.ui.ScanMenuActivity
 import kotlin.jvm.java
 
 class LaunchPageActivity : AppCompatActivity() {
@@ -76,7 +76,7 @@ class LaunchPageActivity : AppCompatActivity() {
 
 
     private fun toMainActivity(){
-        val intent = Intent(this, MainMobileDataActivity::class.java)
+        val intent = Intent(this, ScanMenuActivity::class.java)
         startActivity(intent)
     }
 
