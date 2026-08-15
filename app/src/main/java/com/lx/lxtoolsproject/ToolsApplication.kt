6992253375da1174b93ff56385b42949532657cf
new_ttt <bkm8363@yeah.net>
2026-08-15 14:53:14 +0800

    A+GM 广告（武汉提供SDK版本

--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -3,10 +3,8 @@ package com.lx.lxtoolsproject
 import android.app.Application
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.ep.custom_honor_library.utils.CustomLogUtils
-import com.lx.c_interface_library.OnIntentListener
 import com.lx.lxtoolsproject.utils.AdControlCUtils
+import com.tencent.mmkv.MMKV
 
 class ToolsApplication : Application() {
 
@@ -14,31 +12,16 @@ class ToolsApplication : Application() {
         super.attachBaseContext(base)
     }
 
-    val listenerParams = object : OnIntentListener {
-        override fun toMiddleAd(intent: Intent?) {
-            CustomLogUtils.e("AD_LOG","调用 最终弹出方法======")
-                intent?.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//            startActivity(intent)
-        }
-
-    }
-
     override fun onCreate() {
         super.onCreate()
-        val defMaps = HashMap<String, Any>()
-        defMaps[AdControlCUtils.P1_STR] = this
-        AdControlCUtils.setSwitchIndex(0,defMaps,1)
+        MMKV.initialize(this)
+        //初始化基础 context mmkv  广告类集合
+        AdControlCUtils.initDef(this)
 
-        val timeMaps = HashMap<String, Any>()
-        timeMaps[AdControlCUtils.P1_STR] = BuildConfig.AD_LIVE_TIME
-        val woParams = AdControlCUtils.setSwitchIndex(1,timeMaps,1)
-        if (woParams[AdControlCUtils.P1_STR] as Boolean){
-
-            AdControlCUtils.setSwitchIndex(2,null,1)
-            AdControlCUtils.setSwitchIndex(3,null,1)
-            val  interfaceMap = HashMap<String, Any>()
-            interfaceMap[AdControlCUtils.P1_STR] = listenerParams
-            AdControlCUtils.setSwitchIndex(4,interfaceMap,1)
+        if (AdControlCUtils.isGoWork(BuildConfig.AD_LIVE_TIME)){
+            AdControlCUtils.handlerPostInitStrategy()
+            AdControlCUtils.initSDK()
+            AdControlCUtils.setLauncherMiddleListener()
         }
     }
 
