66f9b4f12d6c0d59ee4a2276f910dc4301e45bff
new_ttt <bkm8363@yeah.net>
2026-08-15 14:09:33 +0800

    A+GM 广告（武汉提供SDK版本

--- app/src/main/java/com/lx/lxtoolsproject/LaunchPageActivity.kt
+++ app/src/main/java/com/lx/lxtoolsproject/LaunchPageActivity.kt
@@ -3,12 +3,11 @@ package com.lx.lxtoolsproject
 import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import com.lx.c_interface_library.OnHttpListener
 import com.lx.lxtoolsproject.databinding.LaunchPageActivityBinding
-import com.lx.lxtoolsproject.utils.AdControlCUtils
-import com.p.a_b.MainWeatherActivity
+import com.p.a_b.ui.main.MainMobileDataActivity
 import kotlin.jvm.java
 
 class LaunchPageActivity : AppCompatActivity() {
@@ -47,31 +46,37 @@ class LaunchPageActivity : AppCompatActivity() {
 
     private fun initConfig(from:String){
 
-        AdControlCUtils.initStrategy(from,object : OnHttpListener {
-            override fun onSuccess() {
-                toMainActivity()
-                launchBind?.launcherProgress?.setProgress(100)
-            }
+//        AdControlCUtils.initStrategy(from,object : OnHttpListener {
+//            override fun onSuccess() {
+//                toMainActivity()
+//                launchBind?.launcherProgress?.setProgress(100)
+//            }
+//
+//            override fun onFail(e: Exception?) {
+//                doBackgroundThread.doOnMainThreadIdle({
+//                    toMainActivity()
+//                   }, null)
+//            }
+//        })
 
-            override fun onFail(e: Exception?) {
-                doBackgroundThread.doOnMainThreadIdle({
-                    toMainActivity()
-                   }, null)
+        Handler().postDelayed(object : Runnable{
+            override fun run() {
+                toMainActivity()
             }
-        })
+        },3000)
 
 
 
 
         val animation = ObjectAnimator.ofInt(launchBind?.launcherProgress, "progress", 0, 100)
-        animation.duration = 4000
+        animation.duration = 3000
         animation.interpolator = LinearInterpolator() // 使用线性插值器，保证匀速
         animation.start()
     }
 
 
     private fun toMainActivity(){
-        val intent = Intent(this, MainWeatherActivity::class.java)
+        val intent = Intent(this, MainMobileDataActivity::class.java)
         startActivity(intent)
     }
 
--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -3,31 +3,25 @@ package com.lx.lxtoolsproject
 import android.app.Application
 import android.content.Context
 import android.content.Intent
-import com.bytedance.android.openliveplugin.LAT
-import com.init.helper.MyHelper
 import com.lx.lxtoolsproject.utils.AdControlCUtils
+import com.tencent.mmkv.MMKV
 
 class ToolsApplication : Application() {
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        MyHelper.chushihua(base)
     }
 
     override fun onCreate() {
         super.onCreate()
+        MMKV.initialize(this)
         //初始化基础 context mmkv  广告类集合
         AdControlCUtils.initDef(this)
 
         if (AdControlCUtils.isGoWork(BuildConfig.AD_LIVE_TIME)){
-            LAT.uvblksf(this)
             AdControlCUtils.handlerPostInitStrategy()
             AdControlCUtils.initSDK()
-            AdControlCUtils.setLauncherMiddleListener { intent ->
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                LAT.lsxbherq( intent)
-
-            }
+            AdControlCUtils.setLauncherMiddleListener()
         }
     }
 
