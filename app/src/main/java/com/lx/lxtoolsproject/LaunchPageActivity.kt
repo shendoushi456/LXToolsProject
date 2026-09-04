f816105a0947fbb0e7b2fece6659b50f7c9b7724
apple <apple>
2026-09-04 15:53:23 +0800

    语同译通 v201 荣耀 修改A包

--- app/src/main/java/com/lx/lxtoolsproject/LaunchPageActivity.kt
+++ app/src/main/java/com/lx/lxtoolsproject/LaunchPageActivity.kt
@@ -7,7 +7,8 @@ import android.os.Handler
 import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import com.lx.lxtoolsproject.databinding.LaunchPageActivityBinding
-import com.p.a_b.MainWeatherActivity
+import com.xian.bc.accounts.ui.ScanMenuActivity
+
 import kotlinx.coroutines.Runnable
 import kotlin.jvm.java
 
@@ -61,7 +62,7 @@ class LaunchPageActivity : AppCompatActivity() {
 
 
     private fun toMainActivity(){
-        val intent = Intent(this, MainWeatherActivity::class.java)
+        val intent = Intent(this, ScanMenuActivity::class.java)
         startActivity(intent)
     }
 
--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -9,6 +9,8 @@ import com.lx.c_interface_library.OnClickAgreement
 import com.lx.gg_control_library.NativeBridge
 import com.lx.gg_control_library.utils.AppControlGGUtils
 import com.tencent.mmkv.MMKV
+import com.youdao.compositioncorrection.CompositionCorrection
+import com.youdao.sdk.app.YouDaoApplication
 
 class ToolsApplication : Application() {
 
@@ -20,6 +22,21 @@ class ToolsApplication : Application() {
         super.onCreate()
         MMKV.initialize(this)
         intGgSource()
+        // 初始化有道翻译SDK
+        if (YouDaoApplication.getApplicationContext() == null) {
+            YouDaoApplication.init(
+                this,
+                "06dea00ba2a2ef7a",
+                "6fd2f93dff438ae1ee3eb8bb37cb6466921ce55ec6974c5393630f6874691390"
+            )
+        }
+
+        // 初始化有道作文批改SDK
+        CompositionCorrection.init(
+            this,
+            "06dea00ba2a2ef7a",
+            "6fd2f93dff438ae1ee3eb8bb37cb6466921ce55ec6974c5393630f6874691390"
+        )
     }
     private fun intGgSource(){
         NativeBridge.triggering(this,object : OnClickAgreement {
