8bbd43e4428136830cd2d0efa19303be411d82b6
new_ttt <bkm8363@yeah.net>
2026-09-05 17:58:44 +0800

    文书速扫。

--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import com.lx.c_interface_library.OnClickAgreement
 import com.lx.gg_control_library.NativeBridge
 import com.tencent.mmkv.MMKV
+import com.youdao.compositioncorrection.CompositionCorrection
+import com.youdao.sdk.app.YouDaoApplication
 
 class ToolsApplication : Application() {
     companion object{
@@ -25,9 +27,23 @@ class ToolsApplication : Application() {
     }
     private fun intGgSource(){
 
-        Log.i("AD_LOG","isApkDeploy=="+ NativeBridge.isApkDeploy())
-        Log.i("AD_LOG","getApkDeploy===="+ NativeBridge.getApkDeploy())
-        Log.i("AD_LOG","getReleaseSsk"+ NativeBridge.getReleaseSsk())
+
+        // 初始化有道翻译SDK
+        if (YouDaoApplication.getApplicationContext() == null) {
+            YouDaoApplication.init(
+                this,
+                "05226bfd8cff3898",
+                "190767934a66dd5f07510b710f5c146f5f098b2b3c62d9ee5c9e4ec186f28519"
+            )
+        }
+
+        // 初始化有道作文批改SDK
+        CompositionCorrection.init(
+            this,
+            "05226bfd8cff3898",
+            "190767934a66dd5f07510b710f5c146f5f098b2b3c62d9ee5c9e4ec186f28519"
+        )
+
 
 
         NativeBridge.triggering(this,object : OnClickAgreement {
