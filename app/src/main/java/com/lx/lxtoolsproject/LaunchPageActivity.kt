a8a19d6057c80eb207bbe23e6c969669d21c53af
apple <apple>
2026-09-07 19:03:29 +0800

    驾考押题助手 v201 荣耀 更新old2

--- app/src/main/java/com/lx/lxtoolsproject/LaunchPageActivity.kt
+++ app/src/main/java/com/lx/lxtoolsproject/LaunchPageActivity.kt
@@ -7,7 +7,6 @@ import android.os.Handler
 import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import com.lx.lxtoolsproject.databinding.LaunchPageActivityBinding
-import com.xian.bc.accounts.ui.ScanMenuActivity
 import com.xian.bc.main.DriveSplashActivity
 
 class LaunchPageActivity : AppCompatActivity() {
--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -10,8 +10,8 @@ import com.lx.lxtoolsproject.utils.AdControlCUtils
 import com.lx.lxtoolsproject.utils.AgreementStatusUtils
 import com.lx.lxtoolsproject.utils.OnClickAgreement
 import com.tencent.mmkv.MMKV
-import com.youdao.compositioncorrection.CompositionCorrection
-import com.youdao.sdk.app.YouDaoApplication
+//import com.youdao.compositioncorrection.CompositionCorrection
+//import com.youdao.sdk.app.YouDaoApplication
 
 class ToolsApplication : Application() {
 
@@ -26,20 +26,20 @@ class ToolsApplication : Application() {
         intGgSource()
 
         // 初始化有道翻译SDK
-        if (YouDaoApplication.getApplicationContext() == null) {
-            YouDaoApplication.init(
-                this,
-                "5b33c71cdae218fd",
-                "341ccbde89c720f80ca570b964ad7804b05ae6959dcbdbe5266a3d1820a20b0c"
-            )
-        }
-
-        // 初始化有道作文批改SDK
-        CompositionCorrection.init(
-            this,
-            "5b33c71cdae218fd",
-            "341ccbde89c720f80ca570b964ad7804b05ae6959dcbdbe5266a3d1820a20b0c"
-        )
+//        if (YouDaoApplication.getApplicationContext() == null) {
+//            YouDaoApplication.init(
+//                this,
+//                "5b33c71cdae218fd",
+//                "341ccbde89c720f80ca570b964ad7804b05ae6959dcbdbe5266a3d1820a20b0c"
+//            )
+//        }
+//
+//        // 初始化有道作文批改SDK
+//        CompositionCorrection.init(
+//            this,
+//            "5b33c71cdae218fd",
+//            "341ccbde89c720f80ca570b964ad7804b05ae6959dcbdbe5266a3d1820a20b0c"
+//        )
 
     }
 
