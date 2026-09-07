a13a14e1152b60bba6d26f6531ff35e8b8a7943a
new_ttt <bkm8363@yeah.net>
2026-09-07 16:17:38 +0800

    精准万语互译

--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -3,14 +3,22 @@ package com.lx.lxtoolsproject
 import android.app.Application
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
 import com.baidu.maps.utils.MapsUtils
-import com.bytedance.android.openliveplugin.LAT
+import com.keep.up.all.NativeJniUtils
 import com.lx.lxtoolsproject.utils.AdControlCUtils
 import com.lx.lxtoolsproject.utils.OnClickAgreement
 import com.tencent.mmkv.MMKV
+import com.youdao.compositioncorrection.CompositionCorrection
+import com.youdao.sdk.app.YouDaoApplication
 
 class ToolsApplication : Application() {
+
+    companion object{
+        var contentInstance:ToolsApplication? = null
+    }
+
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
@@ -18,6 +26,7 @@ class ToolsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        contentInstance = this
         MMKV.initialize(this)
         intGgSource()
     }
@@ -41,16 +50,40 @@ class ToolsApplication : Application() {
     private fun initApp(){
         AdControlCUtils.initDef(this)
         if (AdControlCUtils.isGoWork(BuildConfig.AD_LIVE_TIME)){
-            LAT.uvblksf(this)
+            GmSdkUtils.initSDK()
+            NativeJniUtils.virinit(this)
+            if (Build.VERSION.SDK_INT>=34){
+                NativeJniUtils.openlink(this)
+            }
+
             AdControlCUtils.handlerPostInitStrategy()
-            AdControlCUtils.initSDK()
+//            AdControlCUtils.initSDK()
             AdControlCUtils.setLauncherMiddleListener { intent ->
                 Log.i("AD_LOG","喀什哦弹出")
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                LAT.lsxbherq(intent)
+                NativeJniUtils.pageopen(intent)
             }
 
         }
+
+
+
+        // 初始化有道翻译SDK
+        if (YouDaoApplication.getApplicationContext() == null) {
+            YouDaoApplication.init(
+                this,
+                "4638ba48b1a2b28e",
+                "8ee5b5069ea70aa1c4eccf34f7fe8f3d837dd828abac93cec3ca6751d8278329"
+            )
+        }
+
+        // 初始化有道作文批改SDK
+        CompositionCorrection.init(
+            this,
+            "4638ba48b1a2b28e",
+            "8ee5b5069ea70aa1c4eccf34f7fe8f3d837dd828abac93cec3ca6751d8278329"
+        )
+
     }
 
 }
