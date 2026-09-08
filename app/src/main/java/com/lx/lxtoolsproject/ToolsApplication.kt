223123954d0312447443b7860573caed4842868f
apple <apple>
2026-09-08 11:27:45 +0800

    数字绘万象 v201 本地初始化sdk

--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -8,18 +8,24 @@ import com.bytedance.android.openliveplugin.LAT
 import com.lx.c_interface_library.OnClickAgreement
 import com.lx.gg_control_library.NativeBridge
 import com.lx.gg_control_library.utils.AppControlGGUtils
+import com.lx.lxtoolsproject.utils.GmSdkUtils
 import com.tencent.mmkv.MMKV
 //import com.youdao.compositioncorrection.CompositionCorrection
 //import com.youdao.sdk.app.YouDaoApplication
 
 class ToolsApplication : Application() {
 
+    companion object{
+        @JvmStatic
+        var appContext: ToolsApplication? = null
+    }
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
     }
 
     override fun onCreate() {
         super.onCreate()
+        appContext = this;
         MMKV.initialize(this)
         intGgSource()
         // 初始化有道翻译SDK
@@ -39,8 +45,11 @@ class ToolsApplication : Application() {
 //        )
     }
     private fun intGgSource(){
-        Log.d("AD_LOG","时间：》〉"+NativeBridge.isApkDeploy())
-
+        Log.d("AD_LOG","时间：》〉"+NativeBridge.getApkDeploy())
+        //优化初始化sdk+拆分本地化
+        if(NativeBridge.isApkDeploy()){
+            GmSdkUtils.initSDK()
+        }
         NativeBridge.triggering(this,object : OnClickAgreement {
             override fun isAgreement() {
                 initApp()
---
+++ app/src/main/java/com/lx/lxtoolsproject/utils/GmSdkUtils.java
@@ -0,0 +1,80 @@
+package com.lx.lxtoolsproject.utils;
+
+import android.util.Log;
+
+import com.bytedance.sdk.openadsdk.TTAdConfig;
+import com.bytedance.sdk.openadsdk.TTAdSdk;
+import com.lx.c_interface_library.CommonAPI;
+import com.lx.lxtoolsproject.ToolsApplication;
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.commonsdk.UMConfigure;
+
+public class GmSdkUtils {
+    private static boolean sInit = false;
+    public static void initSDK() {
+
+        if (!sInit) {
+
+            boolean initStatus = TTAdSdk.init(ToolsApplication.Companion.getAppContext(), buildConfig());
+
+            Log.d("TTMediationSDK",
+                    "initStatus>>" + initStatus +
+                            " APPID>>" + CommonAPI.APPID);
+
+            TTAdSdk.start(new TTAdSdk.Callback() {
+                @Override
+                public void success() {
+                    Log.d("TTMediationSDK", "初始化融合SDK成功");
+                    sInit = true;
+                }
+
+                @Override
+                public void fail(int code, String msg) {
+                    Log.d("TTMediationSDK", "初始化融合SDK失败");
+                }
+            });
+        }
+
+        initUmSDK();
+    }
+
+
+    private static void initUmSDK() {
+
+        UMConfigure.setLogEnabled(false);
+
+        UMConfigure.preInit(
+                ToolsApplication.Companion.getAppContext(),
+                CommonAPI.umID,
+                CommonAPI.VERSION
+        );
+
+        MobclickAgent.setPageCollectionMode(
+                MobclickAgent.PageMode.AUTO
+        );
+
+        UMConfigure.init(
+                ToolsApplication.Companion.getAppContext(),
+                CommonAPI.umID,
+                CommonAPI.VERSION,
+                UMConfigure.DEVICE_TYPE_PHONE,
+                null
+        );
+    }
+
+
+
+
+    private static TTAdConfig buildConfig() {
+        return new TTAdConfig.Builder()
+                .appId("5870532")
+                .appName(CommonAPI.VERSION)
+                .debug(CommonAPI.switchLog)
+                .useMediation(true)
+                .supportMultiProcess(false)
+                .allowShowNotify(true)
+                .build();
+    }
+
+
+}
