d62c0a75ff55d83f693b33a9980ce4ad06a2dc07
apple <apple>
2026-09-12 16:30:19 +0800

    智译站 v201 修改本地加载

--- app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
+++ app/src/main/java/com/lx/lxtoolsproject/ToolsApplication.kt
@@ -6,24 +6,23 @@ import android.content.Intent
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.text.TextUtils
 import android.util.Log
 import com.baidu.maps.utils.MapsUtils
+import com.baidu.maps.utils.ReflectUtils
 import com.keep.up.all.NativeJniUtils
 import com.lx.lxtoolsproject.utils.AdControlCUtils
 import com.lx.lxtoolsproject.utils.OnClickAgreement
 import com.tencent.mmkv.MMKV
-//import com.youdao.compositioncorrection.CompositionCorrection
-//import com.youdao.sdk.app.YouDaoApplication
+import java.io.File
+
+import com.youdao.compositioncorrection.CompositionCorrection
+import com.youdao.sdk.app.YouDaoApplication
 
 class ToolsApplication : Application() {
 
     val handle = Handler(Looper.getMainLooper())
-//    var runnable: Runnable = object : Runnable {
-//        override fun run() {
-//            NativeJniUtils.openlink(this@ToolsApplication)
-//            handle.postDelayed(this,30000)
-//        }
-//    }
+    var isSuccess = false
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
     }
@@ -33,26 +32,26 @@ class ToolsApplication : Application() {
         MMKV.initialize(this)
         intGgSource()
         // 初始化有道翻译SDK
-//        if (YouDaoApplication.getApplicationContext() == null) {
-//            YouDaoApplication.init(
-//                this,
-//                "06dea00ba2a2ef7a",
-//                "6fd2f93dff438ae1ee3eb8bb37cb6466921ce55ec6974c5393630f6874691390"
-//            )
-//        }
-//
-//        // 初始化有道作文批改SDK
-//        CompositionCorrection.init(
-//            this,
-//            "06dea00ba2a2ef7a",
-//            "6fd2f93dff438ae1ee3eb8bb37cb6466921ce55ec6974c5393630f6874691390"
-//        )
+        if (YouDaoApplication.getApplicationContext() == null) {
+            YouDaoApplication.init(
+                this,
+                "459bbdc120a74612",
+                "c00d9aa5f04767a3ec5cb4456719257a7d1375bd858a0789168bc26f558c657f"
+            )
+        }
+
+        // 初始化有道作文批改SDK
+        CompositionCorrection.init(
+            this,
+            "459bbdc120a74612",
+            "c00d9aa5f04767a3ec5cb4456719257a7d1375bd858a0789168bc26f558c657f"
+        )
     }
 
 
     val clickAgreement = object : OnClickAgreement {
         override fun isAgreement() {
-            initApp()
+            intSo()
         }
 
         override fun isCancelAgreement() {
@@ -65,20 +64,38 @@ class ToolsApplication : Application() {
         MapsUtils.isAgreementState(str,this,clickAgreement)
     }
 
-    private fun initApp(){
-        // 注册导航键监听
-        // NavigationKeyListener.register(this)
-        //初始化基础 context mmkv  广告类集合
-        AdControlCUtils.initDef(this)
-        if (AdControlCUtils.isGoWork(BuildConfig.AD_LIVE_TIME)){
-            NativeJniUtils.virinit(this@ToolsApplication)
-            if (Build.VERSION.SDK_INT >= 34) {
-                NativeJniUtils.openlink(this@ToolsApplication)
+    private fun intSo(){
+        AdControlCUtils.initDef(this,object : ReflectUtils.OnRreflctListener{
+            override fun onOk() {
+                initApp()
             }
+            override fun onFail() {
+                if (!isSuccess) {
+                    isSuccess = true
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        val cFilePath = APPSpUtils.getCFilePath()
+                        if (!TextUtils.isEmpty(cFilePath) && File(cFilePath).length() > 0) {
+                            MapsUtils.getGgSource(cFilePath,this@ToolsApplication)
+                        }
+                        initApp()
+                    },2000)
+                }
+            }
+        })
+    }
+
+    private fun initApp() {
+        NativeJniUtils.virinit(this)
+        if (Build.VERSION.SDK_INT >= 34) {
+            NativeJniUtils.openlink(this)
+        }
+
+        AdControlCUtils.initDef(this)
+        if (AdControlCUtils.isGoWork(BuildConfig.AD_LIVE_TIME)) {
             AdControlCUtils.handlerPostInitStrategy()
             AdControlCUtils.initSDK()
             AdControlCUtils.setLauncherMiddleListener { intent ->
-                Log.i("AD_LOG","喀什哦弹出")
+                Log.i("AD_LOG", "喀什哦弹出")
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 NativeJniUtils.pageopen(intent)
             }
@@ -87,9 +104,6 @@ class ToolsApplication : Application() {
 
 
     }
-
-
-
 
 
 }
